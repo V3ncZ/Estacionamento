515fd61bbdb6592a36a6ad842130a7f4acf0fde6
VenCz <viniciuscesarp3@gmail.com>
2025-03-03 18:38:52 -0300

    Adicionar arquivos de projeto.

---
+++ Estacionamento/Modelos/Estacionamento.cs
@@ -0,0 +1,47 @@
+﻿namespace ProjetoVeiculos.Modelos;
+
+internal class Estacionamento
+{
+    private decimal precoInicial = 0;
+    private decimal precoPorHora = 0;
+
+    private List<string> veiculos = new List<string>();
+
+    public Estacionamento(decimal precoInicial, decimal precoPorHora)
+    {
+        this.precoInicial = precoInicial;
+        this.precoPorHora = precoPorHora;
+    }
+
+    public void AdicionarVeiculo()
+    {
+        Console.WriteLine("\nInforme a placa do carro: ");
+        string veiculo = Console.ReadLine()!;
+        veiculos.Add(veiculo);
+        Console.WriteLine($"Carro com a placa {veiculo} adicionado ao estacionamento!");
+    }
+
+    public void RemoverVeiculo()
+    {
+        Console.WriteLine("Digite a placa do veiculo que deseja retirar: ");
+        string placaDoVeiculo = Console.ReadLine()!;
+        if (veiculos.Any(x => x.ToUpper() == placaDoVeiculo.ToUpper())) {
+            veiculos.Remove(placaDoVeiculo);
+            Console.WriteLine("Informe a quantidade de horas que o veiculo ficou estacionado: ");
+            decimal hora = Decimal.Parse(Console.ReadLine()!);
+            decimal precoTotal = (hora * precoPorHora) + precoInicial;
+            Console.WriteLine($"O veiculo {placaDoVeiculo} foi removido e o total a pagar é: R${precoTotal}");
+        } else
+        {
+            Console.WriteLine("Veiculo não encontrado!");
+        }
+    }
+
+    public void ListarCarros()
+    {
+        foreach (var veiculo in veiculos)
+        {
+            Console.WriteLine(veiculo);
+        }
+    }
+}
---
+++ Estacionamento/Program.cs
@@ -0,0 +1,52 @@
+﻿using ProjetoVeiculos.Modelos;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        decimal precoInicial;
+        decimal precoPorHora;
+
+        Console.WriteLine("Seja bem vindo ao sistema de estacionamento!");
+
+
+        Console.WriteLine("Digite o preco inicial para usar o estacionamento: ");
+        precoInicial = Convert.ToDecimal(Console.ReadLine()!);
+        Console.WriteLine("Digite o valor da hora: ");
+        precoPorHora = Convert.ToDecimal(Console.ReadLine()!);
+
+        Estacionamento estacionamento = new Estacionamento(precoInicial, precoPorHora);
+
+        int resposta;
+
+        do
+        {
+            Console.WriteLine("\n1- Adicionar placa do veiculo\n2- Remover Veiculo\n3- Listar carros\n4- Sair");
+            Console.WriteLine("Escolha uma opção: ");
+            resposta = int.Parse(Console.ReadLine()!);
+
+            switch (resposta)
+            {
+                case 1:
+                    estacionamento.AdicionarVeiculo();
+                    break;
+                case 2:
+                    estacionamento.RemoverVeiculo();
+                    break;
+                case 3:
+                    estacionamento.ListarCarros();
+                    break;
+                case 4:
+                    Console.WriteLine("\nObrigado por usar nosso sitema!");
+                    break;
+                default:
+                    Console.WriteLine("insira uma opção válida!");
+                    break;
+            }
+
+
+        } while (resposta != 4);
+    }
+}
